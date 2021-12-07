4ec67c696948e92908a45b2f7c2c32d6566e571b
fairye <93123850+fairye@users.noreply.github.com>
2021-12-07 22:50:26 +0300

    Add files via upload

---
+++ EntryTutorial/Views/MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace EntryTutorial.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            throw new NotImplementedException();
+        }
+
+        void OnEntryTextChanged(object sender, TextChangedEventArgs e)
+        {
+            string oldText = e.OldTextValue;
+            string newText = e.NewTextValue;
+        }
+
+        void OnEntryCompleted(object sender, EventArgs e)
+        {
+            string text = ((Entry)sender).Text;
+        }
+    }
+}
